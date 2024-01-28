460e2a1999a72c8f9767ab62a5293797920069c5
al.tomilin <aleksei-tomilin@mail.ru>
2024-01-28 17:16:52 +0300

    clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,23 @@
 const clickerCounter = document.getElementById('clicker__counter');
 let clickerValue = Number(clickerCounter.textContent);
 
+const clickerSpeed = document.getElementById('clicker__speed');
+let lastClicked = new Date();
+let currentClicked = new Date();
+currentClicked = Date.now();
+
 const cookie = document.getElementById('cookie');
+
+let cookieSizeModifier = 20;
 cookie.onclick = () => {
     clickerValue++;
     clickerCounter.textContent = String(clickerValue);
+
+    [lastClicked, currentClicked] = [currentClicked, Date.now()];
+    clickerSpeed.textContent = (1 / ((currentClicked - lastClicked) / 1000)).toFixed(2);
+
+    cookie.width += cookieSizeModifier;
+    cookie.height += cookieSizeModifier;
+    cookieSizeModifier = -cookieSizeModifier;
 
 };
